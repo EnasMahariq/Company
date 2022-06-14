f4386f1f05591526c067eea04b3397ebed799df2
EnasMahariq <mahariqo96@gmail.com>
2022-06-14 21:34:17 +0300

    show admins

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,24 @@
 <?php 
 require("inc/header.php");
 ?>
+<?php
+require("handle/connection.php");
+$query="SELECT COUNT(id) as countAdmins FROM admins";
+$result=mysqli_query($conn,$query);
+$adminsCount=mysqli_fetch_assoc($result);
+
+?>
+
     <div class="container py-5">
         <div class="row">
 
             <div class="col-md-4">
                 <div class="card text-white bg-info mb-3">
-                    <div class="card-header">Products</div>
+                    <div class="card-header">Admins</div>
                     <div class="card-body">
                         <div class="card-text d-flex justify-content-between align-items-center">
-                            <h5>233</h5>
-                          <a href="#" class="btn btn-light">Show</a>
+                            <h5><?=$adminsCount['countAdmins']?></h5>
+                          <a href="admins.php" class="btn btn-light">Show</a>
                         </div>
                     </div>
                 </div>
